d797ba71db06f442c1217e0af31fb53e0f61006e
Jack DeVries <jdevries3133@gmail.com>
2021-09-28 06:15:16 -0400

    !!! squash django migrations

--- grader/models.py
+++ grader/models.py
@@ -19,7 +19,7 @@ from django.db import models
 
 class CourseModel(models.Model):
     name = models.CharField(max_length=255)
-    api_course_id = models.CharField(max_length=50)
+    api_course_id = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
