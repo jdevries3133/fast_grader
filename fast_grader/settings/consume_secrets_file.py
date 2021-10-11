698626ead8a9fd90175d292f2ecb737e98c13b00
Jack DeVries <jdevries3133@gmail.com>
2021-10-11 10:57:46 -0400

    remove unnecessary OAuth scopes

--- fast_grader/settings/consume_secrets_file.py
+++ fast_grader/settings/consume_secrets_file.py
@@ -50,14 +50,12 @@ SOCIALACCOUNT_PROVIDERS = {
         'SCOPE': [
             'email',
             'profile',
-            'https://www.googleapis.com/auth/classroom.coursework.students',
+            'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
             'https://www.googleapis.com/auth/classroom.courses.readonly',
             'https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly',
             'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',
             'https://www.googleapis.com/auth/classroom.rosters.readonly',
-            'https://www.googleapis.com/auth/classroom.profile.emails',
-            'https://www.googleapis.com/auth/classroom.profile.photos',
-            'https://www.googleapis.com/auth/drive'
+            'https://www.googleapis.com/auth/drive.readonly'
         ]
     }
 }
