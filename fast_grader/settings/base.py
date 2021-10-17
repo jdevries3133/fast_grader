292d5870fd185c27ba9fd37a3e9d6ea3866d6bfa
Jack DeVries <jdevries3133@gmail.com>
2021-10-16 22:03:49 -0400

    fix: remove unused django-htmx module

--- fast_grader/settings/base.py
+++ fast_grader/settings/base.py
@@ -81,8 +81,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 
