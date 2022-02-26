08f33a8f9d4b51407cda901adf8a07d572a3b16e
Jack DeVries <jdevries3133@gmail.com>
2022-02-26 11:48:31 -0500

    fix

--- django/infra.tf
+++ django/infra.tf
@@ -22,6 +22,20 @@ terraform {
   }
 }
 
+variable "kube_client_certificate" {
+  type      = string
+  sensitive = true
+}
+
+variable "kube_client_key" {
+  type      = string
+  sensitive = true
+}
+variable "kube_ca_certificate" {
+  type      = string
+  sensitive = true
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
