6a469c62632946c62b857db94c534de855a40bf1
Jack DeVries <jdevries3133@gmail.com>
2022-03-15 20:31:48 -0400

    fix: make abs_url tag respond to `Scheme` header

--- django/extension_support/templatetags.py
+++ django/extension_support/templatetags.py
@@ -12,6 +12,14 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def abs_url(context, view_name, *args, **kwargs):
-    return context["request"].build_absolute_uri(
+    uri: str = context["request"].build_absolute_uri(
         reverse(view_name, args=args, kwargs=kwargs)
     )
+
+    # I don't know why django isn't just handling this case. I think that Django
+    # might be looking for a different header, and that proxy server reconfiguration
+    # might do the trick, but this works too
+    if context['request'].META.get('HTTP_X_SCHEME') == 'https':
+        uri = uri.replace('http://', 'https://')
+
+    return uri
