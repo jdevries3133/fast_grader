97388e17a8eed2a7a438be3cec21407dc5bd07dc
Jack DeVries <jdevries3133@gmail.com>
2022-04-17 21:02:13 -0400

    first attempt at ci/cd

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,9 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "src/**/*"
-  ],
+  content: ["src/**/*"],
   darkMode: "media",
   theme: {
     colors: {
--- extension/upload.js
+++ extension/upload.js
@@ -1,45 +1,68 @@
-const fs = require("fs");
+/**
+ * Script to upload the already-build extension at ./extension.zip to the chrome
+ * webstore using environment variables:
+ *
+ * - DEPLOY_TARGET (see env.template)
+ * - UPLOAD_CLIENT_ID
+ * - UPLOAD_CLIENT_SECRET
+ * - UPLOAD_REFRESH_TOKEN
+ */
 
-const { chromeWebstoreUpload } = require("chrome-webstore-upload");
-const dotenv = require("dotenv");
+(async () => {
+  const fs = require("fs");
 
-dotenv.config();
-
-const buildMode = process.env.DEPLOY_TARGET;
-
-if (buildMode === "dev") {
-  throw new Error(
-    "build mode is set to `dev`; are you sure you want to upload?"
+  const { default: chromeWebstoreUpload } = await import(
+    "chrome-webstore-upload"
   );
-}
+  const dotenv = require("dotenv");
 
-if (["beta", "prod"].includes(buildMode)) {
-  throw new Error(`Invalid build mode: ${buildMode}`);
-}
+  dotenv.config();
 
-const extensionId =
-  process.ENV.DEPLOY_TARGET === "production"
-    ? "abjpdpjdpbcnpflodcknkiebmnglgikp"
-    : "lakppmaadogkmcbgpnaoakhlmdfcnnge";
+  const buildMode = process.env.DEPLOY_TARGET;
 
-const store = chromeWebstoreUpload({
-  extensionId: extensionId,
-  clientSecret: process.ENV.UPLOAD_CLIENT_SECRET,
-  refreshToken: process.ENV.UPLOAD_REFRESH_TOKEN,
-});
+  if (buildMode === "dev") {
+    throw new Error(
+      "build mode is set to `dev`; are you sure you want to upload?"
+    );
+  }
 
-const extension = fs.createReadStream("./extension.zip");
+  if (!["beta", "prod"].includes(buildMode)) {
+    throw new Error(`Invalid build mode: ${buildMode}`);
+  }
 
-store.uploadExisting(extension).then((res) => {
+  const extensionId =
+    buildMode === "prod"
+      ? "abjpdpjdpbcnpflodcknkiebmnglgikp"
+      : "lakppmaadogkmcbgpnaoakhlmdfcnnge";
+
+  const store = chromeWebstoreUpload({
+    extensionId: extensionId,
+    clientId: process.env.UPLOAD_CLIENT_ID,
+    clientSecret: process.env.UPLOAD_CLIENT_SECRET,
+    refreshToken: process.env.UPLOAD_REFRESH_TOKEN,
+  });
+
+  const extension = fs.createReadStream("./extension.zip");
+
   // Response is a Resource Representation
   // https://developer.chrome.com/webstore/webstore_api/items#resource
+  const res = await store.uploadExisting(extension);
+
   if (res.uploadState === "SUCCESS") {
-    store.publish();
+    await store.publish();
     return;
   }
 
-  console.log(res);
+  console.debug("irregular response: ", res);
+
+  if (
+    res.itemError.map((err) => err.error_code).includes("ITEM_NOT_UPDATABLE")
+  ) {
+    throw new Error(
+      "The item cannot be updated now because it is in pending review, ready to publish, or deleted status."
+    );
+  }
   throw new Error(
-    `Upload state is not success :: Upload State: ${uploadState}`
+    `Upload state is not success :: Upload State: ${res.uploadState}`
   );
-});
+})();
